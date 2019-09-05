97e2fb5f262deedc4d8a1ea16a601a0ce2915484
Bob Breznak <bob.breznak@gmail.com>
2019-09-05 09:19:55 -0400

    Add nudging

--- handler.js
+++ handler.js
@@ -2,10 +2,13 @@
 
 import AWS from 'aws-sdk';
 import querystring from 'querystring';
+import { Twilio } from 'twilio';
+import moment from 'moment';
 
 AWS.config.update({region: 'us-east-1'});
 
 const db = new AWS.DynamoDB.DocumentClient();
+const twilio = new Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 
 const helpTextShort = `Request a Checkin with\n"! some optional message"\n\nAnonymous Message with\n"@ some message"\n\nChange your name with\n"$ your name"\n\nAndon Help with\n"?"`;
 const helpTextLong  = `You can Send me "!" at any point and I'll let the hosts to check in with you; If you include a message, I'll pass it along too.\n\n` +
@@ -82,7 +85,8 @@ const anonMessage = async body => {
         messageId: generateId(),
         type: 'ANONYMOUS',
         text: body.Body,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
+        status: 'OPEN',
       }
     };    
     
@@ -104,7 +108,8 @@ const checkInRequest = async (guest, body) => {
         type: 'CHECKIN',
         guestSmsNumber: guest.smsNumber,
         text: body.Body,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
+        status: 'OPEN',
       }
     };    
     
@@ -112,9 +117,47 @@ const checkInRequest = async (guest, body) => {
 
     return 'Got it, sending the request.';
   } catch (error) {
-    console.log('ANON MESSAGE ERROR', error)
+    console.log('ANON MESSAGE ERROR', error);
     return 'Oh no, something went wrong, please wait a moment and try again';
   }
+}
+
+const processAckCode = async (guest, body) => {
+  if(guest.role !== 'TYRANT') {
+    return null;
+  }
+
+  const messageId = body.Body.trim().substring(0, 3).toUpperCase();
+
+  const params = {
+    TableName: process.env.MESSAGES_TABLE,
+    Key: { 
+      messageId,
+    },
+    ConditionExpression: '#messageId = :messageId',
+    UpdateExpression: 'set #status = :status',
+    ExpressionAttributeNames: {
+      '#messageId' : 'messageId',
+      '#status' : 'status' 
+    },
+    ExpressionAttributeValues: {
+      ':messageId' : messageId,
+      ':status' : 'ACKNOWLEDGED'
+    }
+  };
+
+  try {
+    await db.update(params).promise();
+
+    return `Acknowledged ${messageId}`
+  } catch (error) {
+    if(error.code === 'ConditionalCheckFailedException') {
+      return null;
+    }
+
+    console.error('ACK CODE ERROR', error);
+    return 'Something went wrong, please wait a moment and try again.';
+  }  
 }
 
 const processMessage = async body => {
@@ -132,9 +175,24 @@ const processMessage = async body => {
     case '!':
       return checkInRequest(guest, body);
     default:
+      let ackCodeResponse  = await processAckCode(guest, body);
+      if(ackCodeResponse) {
+        return ackCodeResponse;
+      }
+
       return returningGuest(guest);
   };
 };
+
+const sendText = async (to, body) => {
+  const textContent = {
+    to,
+    body,
+    from: process.env.TWILIO_SMS_NUMBER,
+  };
+
+  await twilio.messages.create(textContent);
+}
 
 export const receiveSms = async event => {
   const body = querystring.parse(event.body);
@@ -146,5 +204,106 @@ export const receiveSms = async event => {
   } catch (error) {
     console.error(error);
     return buildMessage('Oh no, something went wrong, please wait a moment and try again');
+  }
+}
+
+export const messageCreated = async event => {
+  console.log(JSON.stringify(event));
+
+  const params = {
+    TableName: process.env.GUESTS_TABLE,
+    KeyConditionExpression: '#role = :role',
+    ExpressionAttributeNames: {
+      '#role': 'role'
+    },
+    ExpressionAttributeValues: {
+      ':role': 'TYRANT'
+    },
+    IndexName: 'role-smsNumber-index'
+  };
+
+  let tyrants = [];
+
+  try {
+    const query = await db.query(params).promise();
+    tyrants = query.Items.map(item => item);
+  } catch (error) {
+    console.log('FETCH TYRANTS', error);
+  }
+
+  for(let record of event.Records) {
+    if(record.eventName === 'INSERT') {
+      const message = {
+        messageId: (record.dynamodb.NewImage.messageId || {}).S,
+        createdAt: (record.dynamodb.NewImage.createdAt || {}).S,
+        text: (record.dynamodb.NewImage.text || {}).S,
+        type: (record.dynamodb.NewImage.type || {}).S,
+        guestSmsNumber: (record.dynamodb.NewImage.guestSmsNumber || {}).S
+      };
+
+      let body = `${message.text}\n\nReply with code "${message.messageId}" to acknowledge`;
+      if(message.type === 'ANONYMOUS') {
+        body = `Anonymous Message:\n${body}`;
+      } else {
+        body = `From ${message.guestSmsNumber}:\n${body}`;
+      }
+
+      for(let tyrant of tyrants) {
+        await sendText(tyrant.smsNumber, body);
+      }
+    }
+  }
+  
+  return true;
+}
+
+export const escalateNotifications = async escalateNotifications => {
+  const threashold = moment().subtract(2, 'minutes').toISOString();
+
+  try {
+    let messagesParams = {
+      TableName : process.env.MESSAGES_TABLE,
+      IndexName: 'status-created_at-index',
+      KeyConditionExpression: '#status = :status AND createdAt < :threashold',
+      ExpressionAttributeNames: {
+        '#status': 'status'
+      },
+      ExpressionAttributeValues: {
+        ':status': 'OPEN',
+        ':threashold': threashold,
+      }
+    };
+
+    const messages = await db.query(messagesParams).promise();
+    console.log('messages', messages);
+
+    if(messages.Items.length === 0) return;
+
+    const captainsParams = {
+      TableName: process.env.GUESTS_TABLE,
+      KeyConditionExpression: '#role = :role',
+      ExpressionAttributeNames: {
+        '#role': 'role'
+      },
+      ExpressionAttributeValues: {
+        ':role': 'CAPTAIN'
+      },
+      IndexName: 'role-index'
+    };
+  
+    const captains = await db.query(captainsParams).promise();
+
+    console.log('captains', captains);
+
+    const messageIds = messages.Items.map(m => m.messageId).join('\n');
+    const body = `Please connect with the Tyrants about unacknowledged messages:\n\n${messageIds}`;
+
+    console.log('messageIds', messageIds);
+
+    for(let captain of captains.Items) {
+      await sendText(captain.smsNumber, body);
+    }
+  } catch (error) {
+    console.log('ERROR MESSAGING ASSISTANTS', error);
   }
 }
